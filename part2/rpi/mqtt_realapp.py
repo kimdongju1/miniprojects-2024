8cba554cb0e15fc99f0636591fdd911535ee9490
kimdongju1 <dkdlkdh@naver.com>
2024-06-12 09:12:38 +0900

    Update mqtt_realapp.py

--- part2/rpi/mqtt_realapp.py
+++ part2/rpi/mqtt_realapp.py
@@ -8,9 +8,11 @@ import board
 import time
 import datetime as dt
 import json
+import io
 
 red_pin = 4
 green_pin = 6
+blue_pin = 5 # RGB 
 dht_pin = 18
 
 dev_id = 'PKNU59'
@@ -20,15 +22,37 @@ loop_num = 0
 
 def onConnect(client, userdata, flags, reason_code, properties):
     print(f'연결성공 : {reason_code}')
-    client.subscribe('pknu/rcv')
+    client.subscribe('pknu/rcv/')
+    # RGB LED 끄기
+    GPIO.output(red_pin, GPIO.HIGH)
+    GPIO.output(green_pin, GPIO.HIGH)
+    GPIO.output(blue_pin, GPIO.HIGH)
+
 
 def onMessage(client, userdata, msg):
-    print(f'{msg.topic} +{msg.payload}')
+    #print(f'{msg.topic} +{msg.payload}')
+    # byte code -> string
+    # json ' -> ""
+    value = json.loads(msg.payload.decode('utf-8').replace("'",'"'))
+    res = (value['control'])
+    if res == 'warning':
+        GPIO.output(blue_pin, GPIO.HIGH) # off
+        GPIO.output(green_pin, GPIO.HIGH) # off
+        GPIO.output(red_pin, GPIO.LOW) # on
+    elif res == 'normal':
+        GPIO.output(blue_pin, GPIO.HIGH) # 
+        GPIO.output(green_pin, GPIO.LOW) # 
+        GPIO.output(red_pin, GPIO.HIGH) # 
+    elif res == 'off':
+        GPIO.output(blue_pin, GPIO.HIGH) # off
+        GPIO.output(green_pin, GPIO.HIGH) # off
+        GPIO.output(red_pin, GPIO.HIGH) # on
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red_pin, GPIO.OUT)
 GPIO.setup(green_pin, GPIO.OUT) # LED 켜는 것
+GPIO.setup(blue_pin, GPIO.OUT)
 GPIO.setup(dht_pin, GPIO.IN) # 온습도값을 RPi에서 받는 것 
 dhtDevice = adafruit_dht.DHT11(board.D18) # 중요!! 
 ## 초기화 끝
@@ -52,13 +76,13 @@ while True:
         print(f'{loop_num} - Temp:{temp}/humid{humd}')
 
         origin_data = { 'DEV_ID' : dev_id,
-                        'CURR_DT' : dt.datetime.now().strftime('$Y-$m-%d %H:%M:%S'),
+                        'CURR_DT' : dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                         'TYPE' : 'TEMPHUMID',
                         'VALUE' : f'{temp}|{humd}'
                       } # dictionary data
         pub_data = json.dumps(origin_data, ensure_ascii=False)
 
-        mqttc.publish('pknu/data', pub_data)
+        mqttc.publish('pknu/data/', pub_data)
         loop_num += 1 
     except RuntimeError as ex:
         print(ex.args[0])
